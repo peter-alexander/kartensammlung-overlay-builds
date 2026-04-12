9183a5e299df08e232b1659205c3e4116db4dc91
peter-alexander <pa78@gmx.at>
2026-04-12 16:17:26 +0200

    Update build-tippecanoe.sh

--- .github/scripts/build-tippecanoe.sh
+++ .github/scripts/build-tippecanoe.sh
@@ -9,8 +9,8 @@ BUILD_DIR="${RUNNER_TEMP:-/tmp}/tippecanoe-src"
 mkdir -p "$INSTALL_DIR"
 rm -rf "$BUILD_DIR"
 
-git clone --depth=1 https://github.com/felt/tippecanoe.git "$BUILD_DIR"
-make -C "$BUILD_DIR" -j"$(nproc)"
+git clone --depth=1 https://github.com/felt/tippecanoe.git "$BUILD_DIR" >&2
+make -C "$BUILD_DIR" -j"$(nproc)" >&2
 cp "$BUILD_DIR/tippecanoe" "$INSTALL_DIR/tippecanoe"
 cp "$BUILD_DIR/tile-join" "$INSTALL_DIR/tile-join"
 chmod +x "$INSTALL_DIR/tippecanoe" "$INSTALL_DIR/tile-join"
